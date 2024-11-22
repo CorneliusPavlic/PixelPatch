964937b59b41970db12a5455c410c40759b127ce
sophia-gh <stesta4@kent.edu>
2024-11-22 16:17:21 -0500

    commented out header
    black and white home page demo, trying things out

--- pixel-patch/src/App.js
+++ pixel-patch/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <div className="app-container">
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
